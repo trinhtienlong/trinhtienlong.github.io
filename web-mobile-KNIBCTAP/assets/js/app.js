5d6fc84f089bdc74928a0da5bc3c69b83db4c4ac
trinhtienlong <trinhtienlong1998@gmail.com>
2022-07-31 02:25:46 +0700

    up-dates-color

--- web-mobile-KNIBCTAP/assets/js/app.js
+++ web-mobile-KNIBCTAP/assets/js/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   $(".image-slider").slick({
-    autoplay: true,
-    autoplaySpeed: 2000,
+    // autoplay: true,
+    // autoplaySpeed: 2000,
     slidesToShow: 4,
     slidesToScroll: 3,
     prevArrow:
@@ -48,8 +48,8 @@ $(document).ready(function () {
   });
 
   $(".banner-slider").slick({
-    autoplay: true,
-    autoplaySpeed: 2000,
+    // autoplay: true,
+    // autoplaySpeed: 2000,
     slidesToShow: 3,
     slidesToScroll: 3,
     prevArrow:
@@ -68,8 +68,8 @@ $(document).ready(function () {
   });
 
   $(".carousel-slider").slick({
-    autoplay: true,
-    autoplaySpeed: 2000,
+    // autoplay: true,
+    // autoplaySpeed: 2000,
     prevArrow:
       "<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left test' aria-hidden='true'></i></button>",
     nextArrow:
@@ -77,8 +77,8 @@ $(document).ready(function () {
   });
 
   $(".product-for").slick({
-    autoplay: true,
-    autoplaySpeed: 2000,
+    // autoplay: true,
+    // autoplaySpeed: 2000,
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: true,
