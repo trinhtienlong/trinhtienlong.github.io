a64b79b89de4951f2380d43b8d4b8d275d3b4839
trinhtienlong <trinhtienlong1998@gmail.com>
2023-03-07 02:15:04 +0700

    update

--- cv-of-me/cv-index.js
+++ cv-of-me/cv-index.js
@@ -5,7 +5,9 @@ let rocket = document.querySelector(".rocket");
 let menu = document.querySelector(".menu");
 let header = document.querySelector("header");
 let navbarLeft = document.querySelectorAll(".navbar-left");
-let mask = document.getElementById("mask")
+let mask = document.getElementById("mask");
+let imgZoom = document.querySelector(".user_img");
+let imgBox = document.querySelector(".img-box");
 
 function scrollRocket() {
   if (
@@ -24,8 +26,38 @@ function toggle(){
   header.classList.toggle("toggle");
   mask.classList.toggle("mask-visible");
 }
-
 menu.onclick = toggle
 mask.onclick = toggle
 
+function checkClick(){
+  navbarLeft.forEach((item) =>{
+    item.onclick = () =>{
+      navbarLeft.forEach((itemChild)=>{
+        if( itemChild.getAttribute("class") !== "navbar-left"){
+          itemChild.className = "navbar-left"
+        }
+      })
+      item.classList.add("navbar-left_bgyellow")
+    }
+  })
+}
+checkClick()
+
+function clickimg(){
+  let src = imgZoom.getAttribute("src")
+  imgBox.classList.add("mask-visible")
+  imgBox.innerHTML = 
+`<div class="img-zoom">
+  <img class="img-zoom_child w-100" src=${src} alt="">
+</div>`
+
+  imgBox.onclick = (e) =>{
+    if( e.target === e.currentTarget ){
+      e.currentTarget.innerHTML = ""
+      e.currentTarget.classList.remove("mask-visible")
+    }
+  }
+}
+
+imgZoom.onclick = clickimg
 
